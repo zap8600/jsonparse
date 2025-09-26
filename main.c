f8130d389fdd19130ccd14b6f72f7e362b284dff
zap8600 <zapost86@gmail.com>
2025-09-26 14:58:30 -0500

    it can get keys now, but nothing else

--- main.c
+++ main.c
@@ -3,6 +3,11 @@
 #include <stdlib.h>
 #include <string.h>
 
+typedef union json_value {
+    char* string;
+    signed long int number;
+} json_value;
+
 int main(int argc, char** argv) {
     if(argc < 2) {
         fprintf(stderr, "Usage: %s [JSON data] [key to find]\n", argv[0]);
@@ -28,5 +33,45 @@ int main(int argc, char** argv) {
     if(*json_data == '\0') {
         printf("Empty JSON object!\n");
         return 0;
+    }
+
+    char** keys = NULL;
+    size_t keys_len = 0;
+    //
+
+    while(1) { // TODO: Handle whitespace
+        char* key = strchr(json_data, '"');
+        if(key++ == NULL) { // TODO: properly handle this
+            fprintf(stderr, "No keys but not empty!\n");
+        }
+        char* key_start = key;
+        size_t key_len = 0;
+        
+        keys = (char**)realloc(keys, (++keys_len) * sizeof(char*));
+        keys[keys_len - 1] = NULL;
+
+        while(1) {
+            if(*key != '"') {
+                keys[keys_len - 1] = (char*)realloc((keys[keys_len - 1]), (++key_len) + 1);
+                if(*key == '\\') {
+                    switch(*(++key)) { // Made this a switch statement so I can easily add more values later on
+                        case '\\':
+                        case '"':
+                        {
+                            keys[keys_len - 1][key_len - 1] = *(key++);
+                            break;
+                        }
+                    }
+                } else {
+                    keys[keys_len - 1][key_len - 1] = *(key++);
+                }
+            } else {
+                keys[keys_len - 1][key_len] = '\0';
+                key++;
+                break;
+            }
+        }
+
+        // *key should be ':'
     }
 }
